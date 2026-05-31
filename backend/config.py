e98c0bd451a8fc413428756b263a42df6083e45b
Axel Rosenquist <axel.rosenquist4@gmail.com>
2026-05-31 20:53:05 +0200

    Added config import

---
+++ backend/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+load_dotenv()
+
+class Settings(BaseModel):
+    DB_HOST: str = os.getenv("DB_HOST")
+    DB_NAME: str = os.getenv("DB_NAME")
+    DB_PORT: str = os.getenv("DB_PORT")
+    DB_USERNAME: str = os.getenv("DB_USERNAME")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
+
+    CORS_ORIGINS: list[str] = os.getenv("CORS_ORIGINS", "").split(",")
+
+
+settings = Settings()
